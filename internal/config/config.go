f393551901bf81a3d079c18b106bc8b1f7f75f48
zhangzhicheng <zhangzhicheng@xl.com>
2026-02-11 15:52:42 +0800

    {"url":"http://192.168.2.253:8079/git/sumbit/","parameter":"{\"msg\":\"\",\"git\":\"zhangzhicheng@xl.com\",\"url\":\"https://code.xl.com/iot/mpc.git\",\"branch\":\"master\",\"plans\":[{\"id\":\"a6050fe5576ef58577331d7a46779ade\",\"msg\":\"feature:update\",\"check\":false,\"time\":\"\"}]}"}

--- internal/config/config.go
+++ internal/config/config.go
@@ -29,13 +29,15 @@ type RedisWrapper struct {
 	Data    json.RawMessage `json:"data"`
 }
 
-var IotCfgStore atomic.Value                  // 存储 Config
-var CfgChangeCh = "CFG_CHANGE"                // 配置数据变更频道
-var DeviceCfg = "IOT:DEVICE"                  // redis存储设备信息的key
-var ProductCfg = "IOT:PRODUCT"                // redis存储产品模型的key
-var SetDeviceProperty = "SET_DEVICE_PROPERTY" // 控制指令下发频道
-var SendCmdResCh = "SEND_CMD_RES"             // 控制指令回执频道
-var ServerStatus = "server:status:mpc"        // redis存储服务心跳的key
+var IotCfgStore atomic.Value   // 存储 Config
+var CfgChangeCh = "CFG_CHANGE" // 配置数据变更频道
+var DeviceCfg = "IOT:DEVICE"   // redis存储设备信息的key
+var ProductCfg = "IOT:PRODUCT" // redis存储产品模型的key
+// var SetDeviceProperty = "SET_DEVICE_PROPERTY" // 控制指令下发频道
+// var SendCmdResCh = "SEND_CMD_RES"                  // 控制指令回执频道
+var SetDeviceProperty = "set_device_current_value" // 控制指令下发频道
+var SendCmdResCh = "device_current_value_response" // 控制指令回执频道
+var ServerStatus = "server:status:mpc"             // redis存储服务心跳的key
 
 func Load(path string) (*Config, error) {
 	v := viper.New()
